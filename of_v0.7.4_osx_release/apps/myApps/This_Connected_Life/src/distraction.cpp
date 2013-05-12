a1d294f82a78cd7c7c6e818c5566631b63e0f4e7
J. Matthew Griffis <grifj153@newschool.edu>
2013-05-11 22:05:37 -0400

    Created TCL project.

---
+++ of_v0.7.4_osx_release/apps/myApps/This_Connected_Life/src/distraction.cpp
@@ -0,0 +1,31 @@
+//
+//  distraction.cpp
+//  This_Connected_Life
+//
+//  Created by J. Matthew Griffis on 5/11/13.
+//
+//
+
+#include "ofMain.h"
+#include "distraction.h"
+
+//--------------------------------------------------------------
+void distraction::setup() {
+    
+    
+    
+}
+
+//--------------------------------------------------------------
+void distraction::update() {
+    
+    
+    
+}
+
+//--------------------------------------------------------------
+void distraction::draw() {
+    
+    
+    
+}
---
+++ of_v0.7.4_osx_release/apps/myApps/This_Connected_Life/src/distraction.h
@@ -0,0 +1,22 @@
+//
+//  distraction.h
+//  This_Connected_Life
+//
+//  Created by J. Matthew Griffis on 5/11/13.
+//
+//
+
+#ifndef __This_Connected_Life__distraction__
+#define __This_Connected_Life__distraction__
+
+class distraction {
+    
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    
+};
+
+#endif /* defined(__This_Connected_Life__distraction__) */
